aaf72866f56412d5df3670dec18a832dfb4b9d0d
Mathieu Dionne <mdionne@cmaisonneuve.qc.ca>
2023-09-13 14:23:49 -0400

    Update GestionnaireFusee.cs

--- Assets/Scripts/Joueur/Tir/GestionnaireFusee.cs
+++ Assets/Scripts/Joueur/Tir/GestionnaireFusee.cs
@@ -41,11 +41,11 @@ public class GestionnaireFusee : NetworkBehaviour
     /*
   * Fonction appeler par le script GestionnaireArmes. Elle gère la fusée tout juste après son spawning
   * Paramètres :
-  * - PlayerRef lanceur : le joueur qui lance la grenade
+  * - PlayerRef lanceur : le joueur qui lance la fusée
   * - NetworkObject lanceurNetworkObject : référence au component NetworkObject du joueur qui lance la fusée
-  * - string nomLanceur : le nom du joueur qui lance la grenade
+  * - string nomLanceur : le nom du joueur qui lance la fusée
   * 1. On mémorise la référence au component NetworkObject.
-  * 2. On mémorise dans des variables : le joueur qui lance la grenade ainsi que son nom et également son component
+  * 2. On mémorise dans des variables : le joueur qui lance la fusée ainsi que son nom et également son component
   * Networkobject. Notez l'utilisation du "this" qui permet de distinguer la variable du paramètre de la fonction qui 
   * porte le même nom.
   * 3.Création d'un timer réseau (TickTimer) d'une durée de 10 secondes
@@ -70,13 +70,14 @@ public class GestionnaireFusee : NetworkBehaviour
     * 3. Première vérification avec un OverlapSphere tout petit (rayon de 0.5 mètre). L'objectif est d'éviter la détection
     * de proximité du joueur qui tire la fusée lorsqu'il vient juste de tirer la fusée
     * 4. On passe à travers la liste et si on détecte la présence de joueur qui tire, on met la variable contactValide à false
-    * 5. On passe à travers la liste des objets détectés par le OverlapSphere. Les objets contenu dans cette liste sont de type
-    * LagCompensatedHit qui est propre à Fusion:
-    * - On tente de récupérer le component (script) gestionnairePointsDeVie de l'objet touché. Renverra null si l'objet n'en possède pas.
-    * - Si l'objet possède un component gestionnairePointsDeVie, c'est qu'il s'agit d'un joueur. On peut alors appeler la fonction
-    * PersoEstTouche() présente dans ce script. On envoie en paramètres le nom du lanceur de grenade et le dommage a appliquer
-    * 
-    * 4.Le serveur fait disparaître (Despawn) la grenade (elle disparaitra sur tous les clients)
+    * 5. Seulement si contactValide est true, on fait un deuxième OverlapSphere avec un rayon plus grand et on passe à travers 
+    * la liste des objets détectés. 
+    * - On tente de récupérer le component (script) gestionnairePointsDeVie de l'objet touché. Renverra null si l'objet n'en 
+    * possède pas.
+    * - Si l'objet possède un component gestionnairePointsDeVie, c'est qu'il s'agit d'un joueur. On peut alors appeler la 
+    * fonction PersoEstTouche() présente dans ce script. On envoie en paramètres le nom du lanceur de fusée et le 
+    * dommage a appliquer
+    * 6.Le serveur fait disparaître (Despawn) la fusée (elle disparaitra sur tous les clients)
     * Pour éviter de répéter plusieurs fois, on s'assure désactive le timer (TickTimer.None)
     */
     public override void FixedUpdateNetwork()
@@ -91,7 +92,7 @@ public class GestionnaireFusee : NetworkBehaviour
                 Runner.Despawn(networkObject);
                 return;
             }
-            /*
+            
             int nbObjetsTouches = Runner.LagCompensation.OverlapSphere(pointDetectionCollision.position, 0.5f, lanceur, infosCollisionsList, layersCollision, HitOptions.IncludePhysX);
 
             //4.
@@ -105,24 +106,30 @@ public class GestionnaireFusee : NetworkBehaviour
                 {
                     //est-ce qu'on se touche soit même
                     if (objetTouche.Hitbox.Root.GetBehaviour<NetworkObject>() == lanceurNetworkObject)
-                        contactValide = true;
+                        contactValide = false;
                 }
             }
             //5.
             if (contactValide)
-            { */
-               int nbObjetsTouches = Runner.LagCompensation.OverlapSphere(pointDetectionCollision.position, 4f, lanceur, infosCollisionsList, layersCollision, HitOptions.None);
+            {
+                nbObjetsTouches = Runner.LagCompensation.OverlapSphere(pointDetectionCollision.position, 4f, lanceur, infosCollisionsList, layersCollision, HitOptions.None);
                 foreach (LagCompensatedHit objetTouche in infosCollisionsList)
                 {
                     GestionnairePointsDeVie gestionnairePointsDeVie = objetTouche.Hitbox.transform.root.GetComponent<GestionnairePointsDeVie>();
                     if (gestionnairePointsDeVie != null)
                         gestionnairePointsDeVie.PersoEstTouche(nomLanceur, 20);
                 }
+                //6.
                 Runner.Despawn(networkObject);
-            //}
+            }
         }
     }
 
+ /*
+  * Override de la fonction Despawned. Fontion exécuté automatiquement sur tous les clients, au moment ou l'objet fusée est
+  * retiré du jeu (Despawned)
+  * Chaque client va instancier localement un système de particule d'explosion à la position déterminer, soit l'avant de la fusée
+  */
     public override void Despawned(NetworkRunner runner, bool hasState)
     {
         Instantiate(prefabExplosion, pointDetectionCollision.position, Quaternion.identity);
